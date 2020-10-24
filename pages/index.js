2b7dbfb519d50f6d963222d2d1928b46aba6a286
kcygan <kacper.cygan@netguru.com>
2020-10-24 20:53:05 +0200

    added desk2 usdz

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       <main>
         <Header title="⭐ Surprise Ricardo ⭐️" />
         Click the gif!
-        <a className="description" rel="ar" href="/desk.usdz">
+        <a className="description" rel="ar" href="/desk2.usdz">
           <img alt="Michael Scott" src="/tenor.gif"/>
         </a>
       </main>
